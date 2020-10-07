b1733c3a427193a91f7765230cc579679aab511b
cjthoma1 <cray.thomas@dcsg.com>
2020-10-06 21:03:38 -0400

    Displaying mock activity data on inbox screen

--- screens/Inbox/index.tsx
+++ screens/Inbox/index.tsx
@@ -1,22 +1,64 @@
+import { Feather, MaterialIcons, FontAwesome } from '@expo/vector-icons';
 import React from 'react';
+import {
+  ActivityAction,
+  ActivityActionButton,
+  ActivityActionText,
+  ActivityBoxAction,
+  ActivityImage,
+  ActivityText,
+  ActivityTextContainer,
+  ActivityTextHeader,
+  BoxAction,
+  Container,
+  Header,
+  Occasion,
+  ScrollView,
+  Title
+} from './styles';
 
-import { Feather, MaterialIcons } from '@expo/vector-icons';
+import server from '../../mock-server.json';
 
-import { Container, Title, Header } from './styles';
-
+interface Activity {
+  id: number;
+  username: string;
+  userImage: string;
+  message: string;
+  live: boolean;
+}
 const Inbox: React.FC = () => {
   return (
     <Container>
       <Header>
         <Title>All activity</Title>
         <MaterialIcons name="arrow-drop-down" size={24} color="black" />
-        <Feather
-          style={{ position: 'absolute', right: 10, top: 10 }}
-          name="send"
-          size={24}
-          color="black"
-        />
+        <BoxAction>
+          <Feather name="send" size={24} color="black" />
+        </BoxAction>
       </Header>
+
+      <ScrollView>
+        <Occasion>This Month</Occasion>
+        {server.inbox.map((activity: Activity) => (
+          <ActivityBoxAction key={activity.id}>
+            <ActivityImage source={{ uri: activity.userImage }} />
+            <ActivityTextContainer>
+              <ActivityTextHeader>{activity.username}</ActivityTextHeader>
+              <ActivityText>{activity.message}</ActivityText>
+            </ActivityTextContainer>
+
+            {activity.live ? (
+              <ActivityAction>
+                <FontAwesome name="chevron-right" size={20} color="#E5E5E5" />
+              </ActivityAction>
+            ) : (
+              <ActivityActionButton>
+                <ActivityActionText>Friends</ActivityActionText>
+              </ActivityActionButton>
+            )}
+          </ActivityBoxAction>
+        ))}
+      </ScrollView>
     </Container>
   );
 };
