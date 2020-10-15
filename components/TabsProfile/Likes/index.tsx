8cc4a94b82195d305b5b3c459a2bbeebabae7f19
cjthoma1 <cray.thomas@dcsg.com>
2020-10-14 20:27:42 -0400

    User profile changes

---
+++ components/TabsProfile/Likes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, TouchableWithoutFeedback, Text } from 'react-native';
+
+import styles from '../styles';
+
+const Likes: React.FC = () => (
+  <View style={styles.content}>
+    <Text style={styles.textBold}>Only you can see which videos you liked</Text>
+    <Text style={styles.text}>
+      You can change this in{' '}
+      <TouchableWithoutFeedback>
+        <Text style={styles.textPrivacySettings}>Privacy settings</Text>
+      </TouchableWithoutFeedback>{' '}
+    </Text>
+  </View>
+);
+
+export default Likes;
---
+++ components/TabsProfile/Posts/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from '../styles';
+
+const Posts: React.FC = () => <View style={styles.content} />;
+
+export default Posts;
